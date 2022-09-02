3a53c31993f2d27c476fafc74e445131eaf33455
Wagner Xu <wagnerhsu@hotmail.com>
2022-09-02 17:21:33 +0800

    add README.Wagner.md

--- src/android/AppAvailability.java
+++ src/android/AppAvailability.java
@@ -13,41 +13,39 @@ import android.content.pm.PackageInfo;
 public class AppAvailability extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if(action.equals("checkAvailability")) {
+        if (action.equals("checkAvailability")) {
             String uri = args.getString(0);
             this.checkAvailability(uri, callbackContext);
             return true;
         }
         return false;
     }
-    
-    // Thanks to http://floresosvaldo.com/android-cordova-plugin-checking-if-an-app-exists
+
+    // Thanks to
+    // http://floresosvaldo.com/android-cordova-plugin-checking-if-an-app-exists
     public PackageInfo getAppPackageInfo(String uri) {
         Context ctx = this.cordova.getActivity().getApplicationContext();
         final PackageManager pm = ctx.getPackageManager();
 
         try {
             return pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
-        }
-        catch(PackageManager.NameNotFoundException e) {
+        } catch (PackageManager.NameNotFoundException e) {
             return null;
         }
     }
-    
+
     private void checkAvailability(String uri, CallbackContext callbackContext) {
 
         PackageInfo info = getAppPackageInfo(uri);
 
-        if(info != null) {
+        if (info != null) {
             try {
                 callbackContext.success(this.convertPackageInfoToJson(info));
-            } 
-            catch(JSONException e) {
-                callbackContext.error("");    
+            } catch (JSONException e) {
+                callbackContext.error(e.toString());
             }
-        }
-        else {
-            callbackContext.error("");
+        } else {
+            callbackContext.error(uri + " is null");
         }
     }
 
